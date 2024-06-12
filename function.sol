16d8a22665d6a2248a50998ca4735de41283e08d
maharajafz03 <maharajafz1999@gmail.com>
2024-06-12 12:57:55 +0530

    .sol

---
+++ function.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+//functions reffer ://datatypes.sol
+
+contract function {
+
+function tap() public view returns (uint) {
+   return block.chainid;
+};
+
+
+function click() public view returns (uint) {
+   return block.number;
+};
+
+};
